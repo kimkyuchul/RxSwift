f5da05d9b24e125266676fe5f1fc2a5039c299a0
김규철 <rbcjf122@gmail.com>
2023-01-06 19:50:10 +0900

    Update: BehaviorSubject 공부

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/BehaviorSubject.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/BehaviorSubject.playground/Contents.swift
@@ -34,6 +34,39 @@ enum MyError: Error {
    case error
 }
 
+let p = PublishSubject<Int>()
+
+p.subscribe { print("PublishSubject >>", $0) }
+    .disposed(by: disposeBag)
+
+//PublishSubject 와 비슷하지만 초기 이벤트를 가진 Subject 입니다. subscribe 될때 가장 최신의 .next 이벤트를 전달
+let b = BehaviorSubject<Int>(value: 0)
+
+b.subscribe { print("BehaviorSubject >>", $0) }
+    .disposed(by: disposeBag)
+
+// BehaviorSubject를 생성하면, 내부에 next 이벤트가 하나 생성된다.
+// 여기에는 생성자로 저장한 값이 전달
+// 새로운 구독자가 추가되면 저장되어있던 넥스트이벤트가 바로 전달된다.
+
+b.onNext(1)
+
+//만약 여기서 새로운 옵저버를 추가하면 BehaviorSubject는 어떤이벤트를 전달 할까?
+
+b.subscribe { print("BehaviorSubject2 >>", $0) }
+    .disposed(by: disposeBag)
+
+// b.onCompleted()
+b.onError(MyError.error)
+
+b.subscribe { print("BehaviorSubject3 >>", $0) }
+    .disposed(by: disposeBag)
+
+
+// BehaviorSubject는 새로운 넥스트 이벤트가 생기면 기존에 있던 이벤트를 교체하는 방식
+// 결과적으로 가장 최신 이벤트를 옵저버에게 전달함.
+// 사용 : 항상 최신 데이터로 채워놓아야 하는 경우에 사용 (유저 프로필)
+
 
 
 
