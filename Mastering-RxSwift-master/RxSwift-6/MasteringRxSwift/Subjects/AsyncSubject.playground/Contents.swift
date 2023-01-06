77f69faf6fe46757b4363c307e7cc5197c6a3047
김규철 <rbcjf122@gmail.com>
2023-01-06 19:50:50 +0900

    Update: AsyncSubject 공부

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/AsyncSubject.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/AsyncSubject.playground/Contents.swift
@@ -34,6 +34,28 @@ enum MyError: Error {
    case error
 }
 
+// 앞서 위에 3개는 서브젝트로 이벤트가 전달되면 즉시 구독자에 전달
+// 반면 AsyncSubject는 서브젝트로 컴플리트 이벤트가 전달되지 전까지 어떠한 이벤트도 전달하지 않음
+// 컴플리트 이벤트가 전달되면 그 시점에서 가장 최근 시점에 전달된 넥스트 이벤트 하나를 구독자에게 전달한다.
+
+let subject = AsyncSubject<Int>()
+
+subject
+    .subscribe { print($0) }
+    .disposed(by: bag)
+
+subject.onNext(1)
+subject.onNext(2)
+subject.onNext(3)
+
+// 가장 최근에 전달된 넥스트 이벤트와 컴플리티드 이벤트 전달됨
+// subject.onCompleted()
+
+subject.onError(MyError.error) // 에러 이벤트에서는 넥스트이벤트가 전달되지 않는다.
+
+// AsyncSuject는 Completed이벤트가 전달된 시점으로 가장 최근에 전달된 하나의next 이벤트를 구독자에게 전달
+// 만약 전달된 이벤트가 없다면 그냥 completed이벤트만 전달하고 종료
+//
 
 
 
