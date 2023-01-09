50cd855a2dc0f4d812c755c68f81fbd1242feaa7
김규철 <rbcjf122@gmail.com>
2023-01-09 21:14:33 +0900

    update: zip CombiningOperator 공부

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CombiningOperators/zip.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CombiningOperators/zip.playground/Contents.swift
@@ -37,6 +37,31 @@ enum MyError: Error {
 let numbers = PublishSubject<Int>()
 let strings = PublishSubject<String>()
 
+// //combineLatest랑 비슷함 하지만 한번 결합된 애는 재방출 하지 않음 -> 첫 번째 요소는 첫 번째 요소와 연결, 두번째 요소는 두 번째 요소와 연결
+// 발생 순서가 같은 Event 끼리 병합하여 방출
+// 이벤트끼리 쌍을 이루지 않으면 방출하지 않음
+
+Observable.zip(numbers, strings) { "\($0) - \($1)" }
+    .subscribe{ print($0) }
+    .disposed(by: bag)
+
+numbers.onNext(1)
+strings.onNext("one")
+
+numbers.onNext(2) // 아직 2와 결합할 짝이없어서 방출 안됨 combineLatest와의 차이점
+strings.onNext("two") // 항상 방출된 순서대로 짝을 맞춤
+
+//numbers.onCompleted() // 하나라도 onCompleted가 전달되면 이후에는 넥스트 이벤트 전달되지 않음.
+numbers.onError(MyError.error)
+strings.onNext("three")
+strings.onCompleted()
+
+let first = Observable.of(1, 2, 3, 4)
+let second = Observable.of("A", "B", "C")
+
+Observable.zip(first, second)
+    .subscribe(onNext: { print("\($0)" + $1) })
+    .disposed(by: bag)
 
 
 
