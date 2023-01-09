7ec053360625368fccfb1860e4cf33901275bd56
김규철 <rbcjf122@gmail.com>
2023-01-09 21:14:02 +0900

    update: merge CombiningOperator 공부

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CombiningOperators/merge.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CombiningOperators/merge.playground/Contents.swift
@@ -24,6 +24,7 @@
 import UIKit
 import RxSwift
 
+// 여러 Observable에 공통된 로직을 실행해야 할 때 merge 해서 Subscribe 할 수 있음
 /*:
  # merge
  */
@@ -38,11 +39,45 @@ let oddNumbers = BehaviorSubject(value: 1)
 let evenNumbers = BehaviorSubject(value: 2)
 let negativeNumbers = BehaviorSubject(value: -1)
 
+// concat과 혼동하지 않기 동작방식이 다르다.
+// concat은 하나의 옵저버블이 completed된다음에 이어지는 옵저버블 방출.
+// merge는 두개이상의 옵저버블을 연결하여 하나의 옵저버블로 합쳐진다음 순서대로 방출한다.
+// merge는 옵저버블이 모두 종료해야 종료된다.
+
+let source = Observable.of(oddNumbers, evenNumbers, negativeNumbers)
+
+source
+    //.merge()
+    .merge(maxConcurrent: 2) // 병합 가능한 옵저버블 수를 제한
+    .subscribe { print($0) }
+    .disposed(by: bag)
+
+oddNumbers.onNext(3)
+evenNumbers.onNext(4)
+
+evenNumbers.onNext(6)
+oddNumbers.onNext(5)
+
+negativeNumbers.onNext(-2) // merge(maxConcurrent: 2) -> 병합 대상에서 제외 ( 머지 연산자는 이런 연산자를 큐에 저장해뒀다가 병합 대상 중 하나가 컴플리티드 이벤트를 전달하면 순서대로 병합 대상에 추가한다. )
+
+oddNumbers.onCompleted() // oddNumbers가 병합 대상에서 제외대고 큐에 저장되어 있던 negativeNumbers(-2)가 추가됨
+
+//oddNumbers.onCompleted() // odd는 이제 새로운 이벤트 전달 불가능 even은가능
+//oddNumbers.onError(MyError.error) // 하나라도 에러라면 그 즉시 중단되고 다음 이벤트는 받지 않는다.
+//
+//evenNumbers.onNext(8)
+//
+//evenNumbers.onCompleted()
+
+// 둘다 종료해야 종료된다.
 
 
+let first = Observable.of(1, 2, 3)
+let second = Observable.of(4, 5, 6)
 
-
-
+Observable.merge(first, second)
+    .subscribe(onNext: { print($0) })
+    .disposed(by: bag)
 
 
 
