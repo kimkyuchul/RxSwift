99d773795bd1e816b3b01f6f863bd20041cde606
김규철 <rbcjf122@gmail.com>
2023-01-13 21:05:18 +0900

    Update: catch, catchAndReturn ErrorHandling 공부

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/ErrorHandling/catch.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/ErrorHandling/catch.playground/Contents.swift
@@ -28,6 +28,9 @@ import RxSwift
  # catch(_:)
  */
 
+//catchError 함수는 넥스트 이벤트나 컴플리트 이벤트는 그대로 전달.
+//하지만 에러 발생 경우 새로운 값을 전달한다.
+//특히나 네트워크 요청에서 많이 사용한다. 올바른 값이 전달되지 않는 상황에서 로컬캐시나 기본값을 사용할 수 있도록 할 수 있다.
 
 let bag = DisposeBag()
 
@@ -39,7 +42,15 @@ let subject = PublishSubject<Int>()
 let recovery = PublishSubject<Int>()
 
 subject
+    .catch { _ in recovery } //에러이벤트 전달 x -> catch 연산자가 원본 서브젝트를 리커버리 서브젝트로 교체했기 때문
     .subscribe { print($0) }
     .disposed(by: bag)
 
+subject.onError(MyError.error)
+subject.onNext(123)
+subject.onNext(11)
 
+recovery.onNext(22)
+recovery.onCompleted()
+
+//catchError는 소스옵저버블에서 생긴 오류를 새로운 옵저버블로 교체하는 역할을 해줌
--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/ErrorHandling/catchAndReturn.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/ErrorHandling/catchAndReturn.playground/Contents.swift
@@ -27,7 +27,7 @@ import RxSwift
 /*:
  # catchAndReturn
  */
-
+// 옵저버블 대신 기본값을 리턴하는 연산자
 let bag = DisposeBag()
 
 enum MyError: Error {
@@ -37,7 +37,14 @@ enum MyError: Error {
 let subject = PublishSubject<Int>()
 
 subject
+    .catchAndReturn(-1) // 파라미터 형식은 소스 옵저버블이 방출하는 형식과 같은 타입(서브젝트 타입)
     .subscribe { print($0) }
     .disposed(by: bag)
 
 subject.onError(MyError.error)
+
+// 에러가 발생했을 때 사용할 기본 값이 있다면 catchAndReturn 연산자를 사용
+// 에러의 종류와 상관없이 항상 동일한 값을 방출한다는 단점이 있음
+// 나머지 경우 catch 연산자를 사용 -> 클로저를 통해 에러 처리 코드를 자유롭게 구현 가능한 장점이 있음
+
+// 두 연산자와 달리 작업을 처음부터 시작하고 싶다면 retry 사용
