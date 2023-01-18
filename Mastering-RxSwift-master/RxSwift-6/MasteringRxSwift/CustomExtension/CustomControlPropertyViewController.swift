130c8c4e972384cb41df2c377e568ff238585e13
김규철 <rbcjf122@gmail.com>
2023-01-18 20:40:09 +0900

    Update: Custom Extention ( Custom ControlProperty )

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/CustomExtension/CustomControlPropertyViewController.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/CustomExtension/CustomControlPropertyViewController.swift
@@ -36,20 +36,47 @@ class CustomControlPropertyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        whiteSlider.rx.value
-            .map { UIColor(white: CGFloat($0), alpha: 1.0) }
+        // value는 ControlProperty이기 때문에 값을 방출하는 옵저버블로 사용할 수 있고, 바안딩 대상이 되는 옵저버가 될 수도 있다.
+        // 바인더는 옵저버블이 아니기 때문에 구현할 수 없음 -> ControlProperty를 커스텀
+        // 쓰기만 필요한 속성 -> 바인더로 구현
+        // 쓰기, 읽기가 모두 필요 속성 -> ControlProperty로 구현
+        
+//        whiteSlider.rx.value
+//            .map { UIColor(white: CGFloat($0), alpha: 1.0) }
+//            .bind(to: view.rx.backgroundColor)
+//            .disposed(by: bag)
+//
+//        resetButton.rx.tap
+//            .map { Float(0.5) }
+//            .bind(to: whiteSlider.rx.value)
+//            .disposed(by: bag)
+//
+//        resetButton.rx.tap
+//            .map { UIColor(white: 0.5, alpha: 1.0) }
+//            .bind(to: view.rx.backgroundColor)
+//            .disposed(by: bag)
+        
+        whiteSlider.rx.color
             .bind(to: view.rx.backgroundColor)
             .disposed(by: bag)
         
         resetButton.rx.tap
-            .map { Float(0.5) }
-            .bind(to: whiteSlider.rx.value)
-            .disposed(by: bag)
-        
-        resetButton.rx.tap
-            .map { UIColor(white: 0.5, alpha: 1.0) }
-            .bind(to: view.rx.backgroundColor)
+            .map { _ in UIColor(white: 0.5, alpha: 1.0) }
+            .bind(to: whiteSlider.rx.color.asObserver(), // 변환된 속성이 컬러에
+                view.rx.backgroundColor.asObserver())
             .disposed(by: bag)
     }
 }
 
+extension Reactive where Base: UISlider {
+    var color: ControlProperty<UIColor?> {
+        return base.rx.controlProperty(editingEvents: .valueChanged, getter: {
+            (slider) in
+            UIColor(white: CGFloat(slider.value), alpha: 1.0)
+        }, setter: { slider, color in
+            var white = CGFloat(1)
+            color?.getWhite(&white, alpha: nil)
+            slider.value = Float(white)
+        })
+    }
+}
