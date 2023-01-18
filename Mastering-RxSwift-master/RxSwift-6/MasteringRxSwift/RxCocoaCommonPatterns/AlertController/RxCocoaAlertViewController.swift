b5f61971e56d2f65059d2c65dc67dbecf2e8b392
김규철 <rbcjf122@gmail.com>
2023-01-18 19:07:21 +0900

    Update: RxCocoa Common Patterns 공부 (AlertController)

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/RxCocoaCommonPatterns/AlertController/RxCocoaAlertViewController.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/RxCocoaCommonPatterns/AlertController/RxCocoaAlertViewController.swift
@@ -42,7 +42,37 @@ class RxCocoaAlertViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        oneActionAlertButton.rx.tap
+        // info 메소드가 리턴하는 옵저버블을 그대로 리턴
+            .flatMap { [unowned self] in self.info(title: "Current Color", message: self.colorView.backgroundColor?.rgbHexString)}
+            .subscribe(onNext: { [unowned self] actionType in
+                switch actionType {
+                case .ok:
+                    print(self.colorView.backgroundColor?.rgbHexString ?? "")
+                default:
+                    break
+                }
+            })
+            .disposed(by: bag)
         
+        twoActionsAlertButton.rx.tap
+            .flatMap { [unowned self] in self.alert(title: "Reset Color", message: "Reset to black color?" )}
+            .subscribe(onNext: { [unowned self] actionType in
+                switch actionType {
+                case .ok:
+                    self.colorView.backgroundColor = UIColor.black
+                default:
+                    break
+                }
+            })
+            .disposed(by: bag)
+        
+        actionSheetButton.rx.tap
+            .flatMap { [unowned self] in self.colorActionSheet(colors: MaterialBlue.allColors, title: "Change color", message: "Choose one")}
+            .subscribe(onNext: { [unowned self] color in
+                self.colorView.backgroundColor = color
+            })
+            .disposed(by: bag)
     }
 }
 
@@ -51,3 +81,85 @@ enum ActionType {
     case cancel
 }
 
+
+extension UIViewController {
+    // 하나의 액션을 표시하는 경고창을 래핑
+    func info(title: String, message: String? = nil) -> Observable<ActionType> {
+        //액션 타입에 추가 작업이 없다면 Observable<Void>, Observable<Completable>로 선언해도 무방
+        
+        return Observable.create { [weak self] (observable) in
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                
+                observable.onNext(.ok)
+                observable.onCompleted()
+            }
+            alert.addAction(okAction)
+            
+            self?.present(alert, animated: true, completion: nil)
+            
+            // 첫번째 차이 : Disposables을 생성해서 리턴하는 것으로 끝나는데, 생성 시점에 클로저를 전달하고 얼럿 컨트롤러를 디스미스 한다.
+            return Disposables.create {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func alert(title: String, message: String? = nil) -> Observable<ActionType> {
+        
+        return Observable.create { [weak self] (observable) in
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                
+                observable.onNext(.ok)
+                observable.onCompleted()
+            }
+            alert.addAction(okAction)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {_ in
+                observable.onNext(.cancel)
+                observable.onCompleted()
+            }
+            alert.addAction(cancelAction)
+            
+            self?.present(alert, animated: true, completion: nil)
+    
+            return Disposables.create {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func colorActionSheet(colors: [UIColor], title: String, message: String? = nil) -> Observable<UIColor> {
+        // 액션 시트에서 컬러를 선택하면 colorView의 background 변경
+        // 그러면 구독자에게 필요한 것은 UIColor -> Observable<UIColor>
+        
+        return Observable.create { [weak self] observable in
+            
+            let actionSheet = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            for color in colors {
+                let colorAction = UIAlertAction(title: color.rgbHexString, style: .default) { _ in
+                    observable.onNext(color)
+                    observable.onCompleted()
+                }
+                actionSheet.addAction(colorAction)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                observable.onCompleted()
+            }
+            actionSheet.addAction(cancel)
+            
+            self?.present(actionSheet, animated: true, completion: nil)
+            
+            return Disposables.create{
+                actionSheet.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
