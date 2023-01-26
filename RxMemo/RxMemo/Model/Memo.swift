35e3b71f248c4a2390a3c689b0547ac6f63c69c9
김규철 <rbcjf122@gmail.com>
2023-01-26 18:00:24 +0900

    Feat: Memo Model 구현

---
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by 김규철 on 2023/01/26.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    // 업데이트된 내용으로 메모 인스턴스를 만들 때 사용
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
