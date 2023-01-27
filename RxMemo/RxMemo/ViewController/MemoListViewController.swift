01dc816ec84f19536b276e082a1299b04ae89217
김규철 <rbcjf122@gmail.com>
2023-01-27 23:41:36 +0900

    Feat: 메모 목록 화면 구현 중

--- RxMemo/RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
     
+    @IBOutlet weak var listTableView: UITableView!
+    
+    
+    @IBOutlet weak var addButton: UIBarButtonItem!
     var viewModel: MemoListViewModel!
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        // 데이터 소스를 구현하지 않아도 됨
+        // 셀 deque 및 재사용을 알아서 해줌
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "cell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag)
     }
 
     override func viewDidLoad() {
