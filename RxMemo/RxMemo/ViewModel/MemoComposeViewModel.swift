5e8bc3b1ec0e34a1913248fe7b8d4a3dc46c6a27
김규철 <rbcjf122@gmail.com>
2023-01-28 15:15:11 +0900

    Feat: MemoComposeViewModel

--- RxMemo/RxMemo/ViewModel/MemoComposeViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -6,7 +6,41 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoComposeViewModel {
+class MemoComposeViewModel: CommonViewModel {
+    // Compose 씬에서 메모를 저장할 속성 -> 새로운 메모를 추가할 때는 nil이 저장되고, 메모를 편집할 때는 편집할 메모가 저장
+    private let content: String?
+    
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    init(title: String, content: String? = nil, sceneCoordinator: SceneCoordinatorType, storage: MemoryStorage, saveAction: Action<String, Void>? = nil, cancelAction: CocoaAction? = nil) {
+        self.content = content
+        
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
     
 }
